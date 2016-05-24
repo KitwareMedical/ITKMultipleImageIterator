5ce1c230a6b81904f57dad0cca24520549d64f4f
Dženan Zukić <dzenan.zukic@kitware.com>
2016-05-24 13:20:16 -0400

    Compiles and passes style check

--- test/DumpIntensities.cxx
+++ test/DumpIntensities.cxx
@@ -1,3 +1,21 @@
+/*=========================================================================
+*
+*  Copyright Insight Software Consortium
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -8,20 +26,20 @@
 // Dumps random samples from files into a csv file
 using namespace std;
 
-int main(int argc, char* argv[])
+int DumpIntensities(int argc, char* argv[])
 {
   if (argc < 2) {
     cerr << "Usage: DumpIntensities file [file ...]" << endl;
     return 1;
   }
-  typedef itk::Image<float,3> ImageType;
-  typedef itk::ImageFileReader<ImageType> ReaderType;
+  typedef itk::Image<float,3>                 ImageType;
+  typedef itk::ImageFileReader<ImageType>     ReaderType;
 
   typedef itk::ImageRegionIterator<ImageType> IteratorType;
   itk::MultipleImageIterator<IteratorType> it;
 
   vector<ImageType::Pointer> images; // Need to keep a reference as iterators only have weak references
-  for (unsigned int i=1; i<argc;++i) {
+  for (int i=1; i<argc; ++i) {
     ReaderType::Pointer r = ReaderType::New();
     r->SetFileName(argv[i]);
     r->Update();
@@ -42,4 +60,5 @@ int main(int argc, char* argv[])
       f << endl;
     }
   }
+  return 0;
 }
