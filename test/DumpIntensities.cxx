705f3841895dc4dc561bf4de3c62c8e343ab073f
Hans Johnson <hans-johnson@uiowa.edu>
2020-02-19 17:02:37 -0600

    ENH: Add .gitattributes to allow running ITK clang-formatting scripts
    ```
    git filter-branch -f \
    --tree-filter "~/ITK/Utilities/Maintenance/clang-format.bash --clang-format ~/Dashboard/src/ITK-clang11/clang-format-Linux --tracked" \
        master..
    ```

--- test/DumpIntensities.cxx
+++ test/DumpIntensities.cxx
@@ -1,20 +1,20 @@
 /*=========================================================================
-*
-*  Copyright Insight Software Consortium
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*         http://www.apache.org/licenses/LICENSE-2.0.txt
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-*
-*=========================================================================*/
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 
 #include <iostream>
 #include <vector>
@@ -27,38 +27,44 @@
 // Dumps random samples from files into a csv file
 using namespace std;
 
-int DumpIntensities(int argc, char* argv[])
+int
+DumpIntensities(int argc, char * argv[])
 {
-  if (argc < 3) {
+  if (argc < 3)
+  {
     cerr << "Usage: DumpIntensities outfile inImage [inImage ...]" << endl;
     return 1;
   }
   using PixelType = unsigned short;
-  using ImageType = itk::Image<PixelType,3>;
+  using ImageType = itk::Image<PixelType, 3>;
   using ReaderType = itk::ImageFileReader<ImageType>;
 
   using IteratorType = itk::ImageRegionIterator<ImageType>;
   itk::MultipleImageIterator<IteratorType> it;
 
   vector<ImageType::Pointer> images; // Need to keep a reference as iterators only have weak references
-  for (int i=2; i<argc; ++i) {
+  for (int i = 2; i < argc; ++i)
+  {
     ReaderType::Pointer r = ReaderType::New();
     r->SetFileName(argv[i]);
     r->Update();
     ImageType::Pointer im = r->GetOutput();
     im->DisconnectPipeline();
     images.push_back(im);
-    it.AddIterator(itk::ImageRegionIterator<ImageType>(im,im->GetLargestPossibleRegion()));
+    it.AddIterator(itk::ImageRegionIterator<ImageType>(im, im->GetLargestPossibleRegion()));
   }
 
   unsigned long long c = 0;
   using Vec3 = itk::FixedArray<PixelType, 3>;
   vector<Vec3> values;
-  for (it.GoToBegin(); !it.IsAtEnd(); ++it, ++c) {
-    if (c % 42 == 0) {
+  for (it.GoToBegin(); !it.IsAtEnd(); ++it, ++c)
+  {
+    if (c % 42 == 0)
+    {
       Vec3 v;
-      for (unsigned int i=0; i<it.Size(); ++i) {
-        v[i]=it[i].Get();
+      for (unsigned int i = 0; i < it.Size(); ++i)
+      {
+        v[i] = it[i].Get();
       }
       values.push_back(v);
     }
@@ -72,9 +78,10 @@ int DumpIntensities(int argc, char* argv[])
   randImage->SetRegions(region);
   randImage->Allocate();
 
-  int index = 0;
+  int                               index = 0;
   itk::ImageRegionIterator<Image1D> oIt(randImage, randImage->GetLargestPossibleRegion());
-  while (!oIt.IsAtEnd()) {
+  while (!oIt.IsAtEnd())
+  {
     oIt.Set(values[index++]);
     ++oIt;
   }
